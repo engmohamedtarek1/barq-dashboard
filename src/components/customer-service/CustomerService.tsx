06c4ada4af6e2a1a7c1aaa84de8ca10948bf13fd
Mohamed Tarek <eng.mohamedtarek0@gmail.com>
2025-10-08 11:32:28 +0300

    fix: update socket connection URL to use the API domain for CustomerService and OrdersTable components

--- src/components/customer-service/CustomerService.tsx
+++ src/components/customer-service/CustomerService.tsx
@@ -61,7 +61,7 @@ export default function CustomerServiceComponent() {
   // Initialize socket connection once
   useEffect(() => {
     const token = getAuthToken();
-    const newSocket = io("barqshipping.com:4000", {
+    const newSocket = io("barqshipping.com", {
       transportOptions: {
         polling: {
           extraHeaders: {
